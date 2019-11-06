66b124d832379336894b67de9d40eeec1a4c2a27
Mosh Feu <moshfeu@gmail.com>
2019-11-07 00:35:39 +0200

    fix icons, add bottom bar, progress with entry ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,18 @@
 <script>
   import 'semantic-ui-css/semantic.css';
   import Timeline from './components/Timeline.svelte';
+  import BottomBar from './components/BottomBar.svelte';
 </script>
 
 <style type="text/scss">
 .app {
-
+  padding: 10px;
 }
 </style>
 
 <div class="app">
   <div class="app-inner">
-    <Timeline></Timeline>
+    <Timeline />
+    <BottomBar />
   </div>
 </div>
---
+++ src/components/BottomBar.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Icon from 'fa-svelte'
+  import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
+</script>
+
+<style type="text/scss">
+  .bottom-bar {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    padding: 5px;
+  }
+</style>
+
+<div class="ui fluid three item menu bottom-bar">
+  <button class="ui circular twitter icon button add">
+    <Icon class="twitter" icon={faPlus}></Icon>
+  </button>
+</div>
